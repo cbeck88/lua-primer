5e8dac0352d3882d5fa5b39f40c9c4c7732a6b1f
Chris Beck <beck.ct@gmail.com>
2016-06-26 19:31:23 -0400

    fix build -- don't cast function object to void *

--- include/primer/support/push_cached.hpp
+++ include/primer/support/push_cached.hpp
@@ -23,26 +23,35 @@ namespace primer {
 
 namespace detail {
 
-template <void (*producer_func)(lua_State *L)>
+template <int (*producer_func)(lua_State *L)>
 void push_cached(lua_State * L) {
-  void * const registry_key = reinterpret_cast<void *>(producer_func);
-  // Just get something usable as a key. It doesn't matter that it is implementation defined.
+  constexpr lua_CFunction registry_key = producer_func;
   // Note that even if the function is inline and defined in a header, C++ guarantees that it
   // has a unique address.
   //
   // The producer function should, without fail, push exactly one object onto the stack.
-  lua_pushlightuserdata(L, registry_key);
+  lua_pushcfunction(L, registry_key);
   if (LUA_TNIL == lua_rawget(L, LUA_REGISTRYINDEX)) {
     PRIMER_ASSERT_STACK_NEUTRAL(L);
     lua_pop(L, 1);
 
     producer_func(L);
     if (lua_gettop(L) && !lua_isnil(L, -1)) {
-      lua_pushlightuserdata(L, registry_key);
+      lua_pushcfunction(L, registry_key);
       lua_pushvalue(L, -2);
       lua_rawset(L, LUA_REGISTRYINDEX);
     }
   }
+}
+
+template <void (*producer_func)(lua_State *L)>
+int wrapped_as_cfunc(lua_State * L) { producer_func(L); return 0; }
+
+template <void (*producer_func)(lua_State * L)>
+void push_cached(lua_State * L) {
+  // Just get something usable as a key that we can comfortably push to lua, the
+  // semantics don't really matter.
+  push_cached<wrapped_as_cfunc<producer_func>>(L);
 }
 
 } // end namespace detail
