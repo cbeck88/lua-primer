5929c818cc08c779be646349ce773d8e57218830
Chris Beck <beck.ct@gmail.com>
2016-07-02 04:00:50 -0400

    commit of generated doc html

--- include/primer/weak_ref.hpp
+++
@@ -1,8 +0,0 @@
-//  (C) Copyright 2015 - 2016 Christopher Beck
-
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#pragma once
-
-#include <primer/weak_ref/weak_ref.hpp>
--- test/lua_internal_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-cd lua-5.3.1-tests && ../stage/lua -e "_port=true" all.lua
