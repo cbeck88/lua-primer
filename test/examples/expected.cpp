6d532cf780f32994538d4fafcfa30409a12736ac
Chris Beck <beck.ct@gmail.com>
2016-07-03 21:01:47 -0400

    add an example executable, add to tests and use it in docs

---
+++ test/examples/expected.cpp
@@ -0,0 +1,35 @@
+#include <primer/expected.hpp>
+
+#include <cassert>
+#include <string>
+
+//[ example_expected
+
+using primer::expected;
+
+expected<std::string> foo(expected<int> e) {
+  if (e) {
+    if (*e >= 7) {
+      return std::string{"woof!"};
+    } else {
+      return primer::error{"bad doggie!"};
+    }
+  } else {
+    return e.err();
+  }
+}
+
+int main() {
+  auto result = foo(6);
+  assert(!result);
+
+  auto result2 = foo(7);
+  assert(result2);
+  assert(*result2 == "woof!");
+
+  auto result3 = foo(primer::error("404"));
+  assert(!result3);
+  assert(result3.err_str() == "404");
+}
+
+//]
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,23 +3,12 @@
 set -e
 
 b2 "$@"
-cd stage && gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./core
-cd ..
 
-cd stage && gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./visitable
-cd ..
-
-cd stage && gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./std
-cd ..
-
-if [ -e stage/boost ]
-then
-  cd stage && gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost
-  cd ..
-fi
-
-if [ -e stage/api ]
-then
-  cd stage && gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./api
-  cd ..
-fi
+for file in stage/*
+do
+  if [[ ${file} != "stage/lua" && ${file} != "stage/persist" && ${file} != "stage/unpersist" && ${file} != "stage/persist.lua" && ${file} != "stage/unpersist.lua" ]]
+  then
+    echo ${file} "..."
+    gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./${file}
+  fi
+done
