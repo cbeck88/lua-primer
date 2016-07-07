e59221e59c6f1feba0c4f36db13167f068dcb643
Chris Beck <beck.ct@gmail.com>
2016-07-07 04:22:55 -0400

    fix a bug in test script which caused runtime errors to be swallowed in CI D:

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,6 +9,6 @@ do
   if [[ ${file} != "stage/lua" && ${file} != "stage/persist" && ${file} != "stage/unpersist" && ${file} != "stage/persist.lua" && ${file} != "stage/unpersist.lua" ]]
   then
     echo ${file} "..."
-    gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./${file}
+    gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./${file}
   fi
 done
