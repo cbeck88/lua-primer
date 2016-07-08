6893973d88b7934ba9e992e3ef92997ed095246c
Chris Beck <beck.ct@gmail.com>
2016-07-08 19:09:01 -0400

    fixup test script -- search for bjam also since ubuntu calls it this

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-b2 "$@"
+if hash b2 2>/dev/null; then
+  b2 "$@"
+elif hash bjam 2>/dev/null; then
+  bjam "$@"
+else
+  echo >&2 "Require b2 or bjam but it was not found. Aborting."
+  exit 1
+fi
 
 for file in stage/*
 do
