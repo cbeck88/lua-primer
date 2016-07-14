c97133df6f091bb31b07706a2cf9a2955b60f0fd
Chris Beck <beck.ct@gmail.com>
2016-07-14 04:46:29 -0400

    add gdb to eris internal tests, try to catch a travis crash

--- test/eris-internal-tests.sh
+++ test/eris-internal-tests.sh
@@ -2,6 +2,6 @@
 set -e
 cd stage
 
-./persist persist.lua test.eris && ./unpersist unpersist.lua test.eris
+gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./persist persist.lua test.eris && gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./unpersist unpersist.lua test.eris
 
 cd ..
