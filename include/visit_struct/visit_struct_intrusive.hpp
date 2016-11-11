5654c1fb16b700bf6db0faca116bed11acb1cfe6
Chris Beck <beck.ct@gmail.com>
2016-11-10 22:48:31 -0500

    update visit_struct

--- include/visit_struct/visit_struct_intrusive.hpp
+++ include/visit_struct/visit_struct_intrusive.hpp
@@ -70,6 +70,46 @@ struct Rank<0> {};
 
 static constexpr int maxVisitableRank = 100;
 
+/***
+ * To create a "compile-time" TypeList whose members are accumulated one-by-one,
+ * the basic idea is to define a function, which takes a `Rank` object, and
+ * whose return type is the type representing the current value of the list.
+ *
+ * That function is not a template function -- it is defined as taking a
+ * particular rank object. Initially, it is defined only for `Rank<0>`.
+ *
+ * To add an element to the list, we define an overload of the function, which
+ * takes the next higher `Rank` as it's argument. It's return value is,
+ * the new value of the list, formed by using `Append_t` with the old value.
+ *
+ * To obtain the current value of the list, we use decltype with the name of the
+ * function, and `Rank<100>`, or some suitably large integer. The C++ standard
+ * specifies that overload resolution is in this case unambiguous and must
+ * select the overload for the "most-derived" type which matches.
+ *
+ * The upshot is that `decltype(my_function(Rank<100>{}))` is a single well-formed
+ * expression, which, because of C++ overload resolution rules, can be a
+ * "mutable" value from the point of view of metaprogramming.
+ *
+ *
+ * Attribution:
+ * I first learned this trick from a stackoverflow post by Roman Perepelitsa:
+ * http://stackoverflow.com/questions/4790721/c-type-registration-at-compile-time-trick
+ *
+ * He attributes it to a talk from Matt Calabrese at BoostCon 2011.
+ *
+ *
+ * The expression is inherently dangerous if you are using it inside the body
+ * of a struct -- obviously, it has different values at different points of the
+ * structure definition. The "END_VISITABLES" macro is important in that this
+ * finalizes the list, typedeffing `decltype(my_function(Rank<100>{}))` to some
+ * fixed name in your struct at a specific point in the definition. That
+ * typedef can only ultimately have one meaning, no matter where else the name
+ * may be used (even implicitly) in your structure definition. That typedef is
+ * what the trait defined in this header ultimately hooks into to find the
+ * visitable members.
+ */
+
 // A tag inserted into a structure to mark it as visitable
 
 struct intrusive_tag{};
@@ -86,13 +126,21 @@ struct member_ptr_helper {
   static constexpr T & apply(S & s) { return s.*member_ptr; }
   static constexpr const T & apply(const S & s) { return s.*member_ptr; }
   static constexpr T && apply(S && s) { return std::move(s.*member_ptr); }
+
+  static constexpr T S::* get_ptr() { return member_ptr; }
 };
 
+// M should be a member_ptr_helper
 template <typename M>
 struct member_helper {
   template <typename V, typename S>
   VISIT_STRUCT_CONSTEXPR static void apply_visitor(V && visitor, S && structure_instance) {
-    visitor(M::member_name, M::apply(std::forward<S>(structure_instance)));
+    std::forward<V>(visitor)(M::member_name, M::apply(std::forward<S>(structure_instance)));
+  }
+
+  template <typename V>
+  VISIT_STRUCT_CONSTEXPR static void apply_visitor(V && visitor) {
+    std::forward<V>(visitor)(M::member_name, M::get_ptr());
   }
 };
 
@@ -105,13 +153,20 @@ struct structure_helper<TypeList<Ms...>> {
   VISIT_STRUCT_CONSTEXPR static void apply_visitor(V && visitor, S && structure_instance) {
     // Use parameter pack expansion to force evaluation of the member helper for each member in the list.
     // Inside parens, a comma operator is being used to discard the void value and produce an integer, while
-    // not being an unevaluated context and having the order of evaluation be enforced by the compiler.
+    // not being an unevaluated context. The order of evaluation here is enforced by the compiler.
     // Extra zero at the end is to avoid UB for having a zero-size array.
-    int dummy[] = { (member_helper<Ms>::apply_visitor(std::forward<V>(visitor), std::forward<S>(structure_instance)), 0)..., 0};
+    int dummy[] = {(member_helper<Ms>::apply_visitor(std::forward<V>(visitor), std::forward<S>(structure_instance)), 0)..., 0};
     // Suppress unused warnings, even in case of empty parameter pack
     static_cast<void>(dummy);
     static_cast<void>(visitor);
     static_cast<void>(structure_instance);
+  }
+
+  template <typename V>
+  VISIT_STRUCT_CONSTEXPR static void apply_visitor(V && visitor) {
+    int dummy[] = {(member_helper<Ms>::apply_visitor(std::forward<V>(visitor)), 0)..., 0};
+    static_cast<void>(dummy);
+    static_cast<void>(visitor);
   }
 };
 
@@ -134,9 +189,17 @@ struct visitable <T,
                                          >::type
                  >
 {
+  // Apply to an instance
+  // S should be the same type as T modulo const and reference
   template <typename V, typename S>
   VISIT_STRUCT_CONSTEXPR static void apply(V && v, S && s) {
     detail::structure_helper<typename T::Visit_Struct_Registered_Members_List__>::apply_visitor(std::forward<V>(v), std::forward<S>(s));
+  }
+
+  // Apply with no instance
+  template <typename V>
+  VISIT_STRUCT_CONSTEXPR static void apply(V && v) {
+    detail::structure_helper<typename T::Visit_Struct_Registered_Members_List__>::apply_visitor(std::forward<V>(v));
   }
 
   static constexpr bool value = true;
