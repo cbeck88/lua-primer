ae49741c6c8a4616daf27b108b8bc7c130795ca1
Chris Beck <beck.ct@gmail.com>
2016-11-17 11:51:44 -0500

    reduce error test case

--- test/error.cpp
+++ test/error.cpp
@@ -11,15 +11,11 @@
 using primer::expected;
 
 expected<std::string>
-foo(expected<int> e) {
-  if (e) {
-    if (*e >= 7) {
-      return std::string{"woof!"};
-    } else {
-      return primer::error{"bad doggie!"};
-    }
+foo(int e) {
+  if (e >= 7) {
+    return std::string{"woof!"};
   } else {
-    return e.err();
+    return primer::error{"bad doggie!"};
   }
 }
 
@@ -30,10 +26,6 @@ int main() {
   auto result2 = foo(7);
   assert(result2);
   assert(*result2 == "woof!");
-
-  auto result3 = foo(primer::error("404"));
-  assert(!result3);
-  assert(result3.err().str() == "404");
 
   std::cout << "OK!" << std::endl;
 }
