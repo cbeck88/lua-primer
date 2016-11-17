0c8ab8fa4e756892aba780a9069ea537bc745150
Chris Beck <beck.ct@gmail.com>
2016-11-17 10:23:04 -0500

    simplify travis and appveyor builds (after stage_lua folder)

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -13,9 +13,6 @@ fi
 
 for file in stage/*
 do
-  if [[ ${file} != "stage/lua" && ${file} != "stage/persist" && ${file} != "stage/unpersist" && ${file} != "stage/persist.lua" && ${file} != "stage/unpersist.lua" ]]
-  then
-    echo ${file} "..."
-    gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./${file}
-  fi
+  echo ${file} "..."
+  gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./${file}
 done
