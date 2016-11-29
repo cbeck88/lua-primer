6b9f8df5f4655df1670953e4dbbddc89670bb893
Chris Beck <beck.ct@gmail.com>
2016-11-29 11:11:41 -0500

    add some tutorial information on userdata

--- test/tutorial.cpp
+++ test/tutorial.cpp
@@ -10,7 +10,7 @@ using uint = unsigned int;
 void push_read() {
 //[ primer_tutorial_push_read_example
 //` The most basic operations in `primer` are `push` and `read`.
-
+//`  
 //` The standard lua C API provides a number of functions which do things like,
 //` push a string onto the stack, test if a value on the stack is a string,
 //` fetch a string from the stack. Push an integer onto the stack, test, fetch,
@@ -76,11 +76,11 @@ void push_read() {
   }
   
 //` This behavior with `std::vector` requires including the header `#include <primer/std/vector.hpp>`.
-
+//`  
 //` There are similar extension headers for other kinds of data structures. (See reference).
-
+//`  
 //` It is also relatively easy to provide overloads for `primer::push` and `primer::read`.
-
+//`  
 //` These two functions are used by many of the more complex features of `primer`, so
 //` customizing them will make primer "aware" of your datatypes more or less everywhere.
 
@@ -113,6 +113,7 @@ void adapt_one() {
 
 //->
   // ...
+
   lua_pushcfunction(L, &is_pythag);
 //<-
   lua_close(L);
@@ -132,14 +133,17 @@ void adapt_two() {
 
 //->
   // ...
+
   lua_pushcfunction(L, PRIMER_ADAPT(&new_is_pythag));
 //<-
   lua_close(L);
 }
 //->
 
-//` Note that PRIMER_ADAPT must be used to "convert" the primer signature
+//` PRIMER_ADAPT is used to "convert" the primer signature
 //` `primer::result (*)(lua_State *, ...)` to the lua signature `int (*)(lua_State *)`.
+
+//`  
 
 //` `PRIMER_ADAPT` generates the boiler-plate which reads the arguments off of the stack,
 //` using `primer::read` to do the work. That is: If you implement `primer::read` for your custom
@@ -147,8 +151,284 @@ void adapt_two() {
 
 //]
 
+namespace test_one {
+
+//[ primer_tutorial_userdata_example_1
+
+//` `userdata` is a special kind of lua value. It cannot be created directly by lua code -- it can only
+//` be created by using C API functions.  
+
+//` A `userdata` is essentially a block of memory, owned by lua,
+//` which external code has initialized in some way. Scripts are able to pass this "value" around, storing
+//` it in variables or passing it to functions.  
+
+//` Here's a minimal example of userdata -- an opaque "token" type for use with some API.
+
+struct token {
+  int id;
+};
+
+int new_token(lua_State * L) {
+  static int count = 0;
+  void * udata = lua_newuserdata(L, sizeof(token));
+  new (udata) token{count++};
+  return 1;
+}
+
+int inspect_token(lua_State * L) {
+  if (void * udata = lua_touserdata(L, 1)) {
+    std::cout << "Token id: " << static_cast<token*>(udata)->id << std::endl;
+  } else {
+    std::cout << "Not a userdata" << std::endl;
+  }
+  return 0;
+}
+
+//<-
+void userdata_test_one() {
+  lua_State * L = luaL_newstate();
+
+//->
+  // ...
+
+  lua_pushcfunction(L, new_token);
+  lua_setglobal(L, "new_token");
+  lua_pushcfunction(L, inspect_token);
+  lua_setglobal(L, "inspect_token");
+
+  const char * script = ""
+  "x = new_token() "
+  "y = new_token() "
+  "inspect_token(x) "
+  "inspect_token(y) "
+  "y = x "
+  "inspect_token(y) "
+  "inspect_token(5) ";
+
+  assert(LUA_OK == luaL_loadstring(L, script));
+  assert(LUA_OK == lua_pcall(L, 0, 0, 0));
+//<-
+//  if(LUA_OK != lua_pcall(L, 0, 0, 0)) {
+//    std::cerr << lua_tostring(L, -1);
+//    std::abort();
+//  }
+//->
+//<-
+  lua_close(L);
+}
+
+} // end anonymous namespace
+//->
+
+//` This example prints
+//`  
+//= Token id: 0  
+//= Token id: 1  
+//= Token id: 0  
+//= Not a userdata  
+//`  
+//`  
+//` While the tokens can be stored in variables, lua can't inspect or change them directly.
+//` `x` cannot be used with lua functions like `+` that take a number, and `x.id` is an error.
+//`  
+//` An issue with the above code is that if your application has multiple userdata types,
+//` then `inspect_token` can lead to undefined behavior -- it uses `static_cast` to convert
+//` any userdata type to a `token`.  
+//`
+//` For this and other reasons, you should always set a metatable associated to any userdata value,
+//` and use `luaL_testudata` to test that it's the userdata type you think you had.
+//`  
+//` Rather than illustrate that, we'll now show a more primer way of doing it.
+
+struct token {
+  int id;
+};
+
+namespace primer {
+namespace traits {
+
+template <>
+struct userdata<token> {
+  static constexpr const char * name = "token";
+};
+
+} // end namespace traits
+} // end namespace primer
+
+//<-
+namespace {
+//->
+
+primer::result new_token(lua_State * L) {
+  static int count = 0;
+  primer::push_udata<token>(L, count++);
+  return 1;
+}
+
+primer::result inspect_token(lua_State *, token & tok) {
+  std::cout << "Token id: " << tok.id << std::endl;
+  return 0;
+}
+
+
+//<-
+void userdata_test_two() {
+  lua_State * L = luaL_newstate();
+
+//->
+  // ...
+
+  lua_pushcfunction(L, PRIMER_ADAPT(&new_token));
+  lua_setglobal(L, "new_token");
+  lua_pushcfunction(L, PRIMER_ADAPT(&inspect_token));
+  lua_setglobal(L, "inspect_token");
+
+  const char * script = ""
+  "x = new_token() "
+  "y = new_token() "
+  "inspect_token(x) "
+  "inspect_token(y) "
+  "y = x "
+  "inspect_token(y)";
+  // "inspect_token(5)";
+
+  assert(LUA_OK == luaL_loadstring(L, script));
+  assert(LUA_OK == lua_pcall(L, 0, 0, 0));
+//<-
+//  if(LUA_OK != lua_pcall(L, 0, 0, 0)) {
+//    std::cerr << lua_tostring(L, -1);
+//    std::abort();
+//  }
+//->
+//<-
+  lua_close(L);
+}
+//->
+
+//` In this example, the userdata type `token` is passed by reference to the function `inspect_token`.
+//` This implicitly carries out the `luaL_testudata`, and signals an argument error if the it isn't passed
+//` a `token`. (i.e. `inspect_token(5)`)
+
+//]
+
+} // end anonymous namespace
+
+namespace test_three {
+
+//[ primer_tutorial_userdata_example_2
+
+//` [h3 Metatables]
+//` Extending our contrived example, let's suppose we want lua scripts to be able to access the token id, but not change it.
+//`  
+//` In pure lua it looks something like this:
+
+struct token {
+  int id;
+};
+
+int impl_token_index(lua_State * L) {
+  token & tok = *static_cast<token*>(luaL_checkudata(L, 1, "token"));
+  if (0 == std::strcmp(lua_tostring(L, 2), "id")) {
+    lua_pushnumber(L, tok.id);
+    return 1;
+  }
+  return 0;
+}
+
+void init_token_metatable(lua_State * L) {
+  luaL_newmetatable(L, "token");
+
+  lua_pushcfunction(L, impl_token_index);
+  lua_setfield(L, -2, "__index");
+
+  lua_pop(L, 1);
+}
+
+int new_token(lua_State * L) {
+  static int count = 0;
+  void * udata = lua_newuserdata(L, sizeof(token));
+  new (udata) token{count++};
+  luaL_setmetatable(L, "token");
+  return 1;
+}
+
+int inspect_token(lua_State * L) {
+  token & tok = *static_cast<token*>(luaL_checkudata(L, 1, "token"));
+  std::cout << "Token id: " << tok.id << std::endl;
+  return 0;
+}
+
+
+//<-
+void userdata_test_three() {
+  lua_State * L = luaL_newstate();
+  luaL_requiref(L, "", &luaopen_base, 1);
+//->
+  // ...
+
+  init_token_metatable(L);
+
+  lua_pushcfunction(L, new_token);
+  lua_setglobal(L, "new_token");
+  lua_pushcfunction(L, inspect_token);
+  lua_setglobal(L, "inspect_token");
+
+  const char * script = ""
+  "x = new_token() "
+  "y = new_token() "
+  "inspect_token(x) "
+  "inspect_token(y) "
+  "if x.id < y.id then print('x < y') end "
+  "y = x "
+  "if x.id == y.id then print('x == y') end ";
+
+//=  assert(LUA_OK == luaL_loadstring(L, script));
+//<-
+  if(LUA_OK != luaL_loadstring(L, script)) {
+    std::cerr << lua_tostring(L, -1);
+    std::abort();
+  }
+//->
+
+//=  assert(LUA_OK == lua_pcall(L, 0, 0, 0));
+//<-
+  if(LUA_OK != lua_pcall(L, 0, 0, 0)) {
+    std::cerr << lua_tostring(L, -1);
+    std::abort();
+  }
+//->
+//<-
+  lua_close(L);
+}
+//->
+
+//` This example prints
+//`  
+//= Token id: 0  
+//= Token id: 1  
+//= x < y
+//= x == y
+//`  
+//` When using the pure lua C api.
+
+//]
+
+} // end namespace test_three
+
+//[ primer_tutorial_userdata_example_3
+
+//` However scripts cannot directly inspect the memory or otherwise access it, unless functions
+//` are provided in order to do so. Your C++ functions are able to access it however they like.
+
+//` You can create an object-oriented interface by giving the userdata a *metatable*.  
+
+//]
+
 int main() {
   push_read();
   adapt_one();
   adapt_two();
+  test_one::userdata_test_one();
+  userdata_test_two();
+  test_three::userdata_test_three();
 }
